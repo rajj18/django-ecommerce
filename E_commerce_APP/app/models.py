dfbfe30a05b97fe4b432aced461b010918f54b67
Raj Jajme <rajfb18@gmail.com>
2024-01-24 12:02:26 +0530

    second commit

--- E_commerce_APP/app/models.py
+++ E_commerce_APP/app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -13,6 +14,43 @@ CATEGORY_CHOICES = (
     ('IC', 'Ice-cream')
 )
 
+STATE_CHOICES = (
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chhattisgarh', 'Chhattisgarh'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'Himachal Pradesh'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharashtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odisha', 'Odisha'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('Uttarakhand', 'Uttarakhand'),
+    ('West Bengal', 'West Bengal'),
+    ('Andaman and Nicobar Islands', 'Andaman and Nicobar Islands'),
+    ('Chandigarh', 'Chandigarh'),
+    ('Dadra and Nagar Haveli and Daman and Diu', 'Dadra and Nagar Haveli and Daman and Diu'),
+    ('Lakshadweep', 'Lakshadweep'),
+    ('Delhi', 'Delhi'),
+    ('Puducherry', 'Puducherry'),
+)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -26,3 +64,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+
+    def __str__(self):
+        return self.name
--- E_commerce_APP/app/views.py
+++ E_commerce_APP/app/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
-from . models import Product
-from . forms import CustomerRegistrationForm
+from . models import Product, Customer
+from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
 
@@ -59,3 +59,32 @@ class CustomerRegistrationView(View):
         else:
             messages.warning(request, 'Invalid Input Data')
         return render(request, 'app/customerregistration.html', locals())
+
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', locals())
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, "Profile saved successfully")
+        else:
+            messages.warning(request, 'Invalid input data')
+        return render(request, 'app/profile.html', locals())
+
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', locals())
